300a24edfe2dd3d1eb7abe64b3441079f7544646
Tuncay Candan <246895418+tuncaycandan@users.noreply.github.com>
2026-08-16 21:42:41 +0300

    Add application metadata

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,8 +1,11 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("VoidErase")]
-[assembly: AssemblyDescription("VoidErase")]
+[assembly: AssemblyDescription("Secure permanent file and folder erasure utility")]
 [assembly: AssemblyProduct("VoidErase")]
+[assembly: AssemblyCompany("tuncay.net.tr")]
+[assembly: AssemblyCopyright("Copyright © 2026 Tuncay Candan")]
+[assembly: AssemblyTrademark("")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
